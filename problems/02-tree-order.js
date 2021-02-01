064cbb82d918181a88a0443c394a1991617f7589
Icarus <ibuckhold@gmail.com>
2021-02-01 16:12:34 -0700

    Finished prob 1 + 2

--- problems/02-tree-order.js
+++ problems/02-tree-order.js
@@ -1,17 +1,63 @@
 // Implement a function that takes the root of a Binary Tree
 // and returns an array containing the values from a pre-order
 // traversal.
-function preOrderArray(root) {
+
+class TreeNode {
     // Your code here
+    constructor(val) {
+        this.val= val;
+        this.left= null;
+        this.right= null;
+    }
+
 }
+
+function preOrderArray(root, arr=[]) {
+    if (root === null) return [];
+    arr.push(root.val);
+    preOrderArray(root.left, arr);
+    preOrderArray(root.right, arr);
+    return arr;
+}
+root = new TreeNode('a');
+        let b = new TreeNode('b');
+        let c = new TreeNode('c');
+        let d = new TreeNode('d');
+        let e = new TreeNode('e');
+        let f = new TreeNode('f');
+
+        root.left = b;
+        root.right = c;
+        b.left = d;
+        b.right = e;
+        c.right = f;
 
 // Implement a function that takes the root of a Binary Tree
 // and returns an array containing the values from an in-order
 // traversal.
 function inOrderArray(root) {
-    // Your code here
+    if (root===null) return [];
+    // if (root.left) {
+    //     inOrderArray(root.left);
+    // }else {
+    //     arr.push(root.val);
+    // }
+    // arr.push(root.val);
+    // if (root.right) {
+    //     inOrderArray(root.right);
+    // }else {
+    //     arr.push(root.val);
+    // }
+    // return arr;
+
+    return [
+        ...inOrderArray(root.left),
+        root.val,
+        ...inOrderArray(root.right)
+    ]
 }
 
+console.log(inOrderArray(root))
 // Implement a function that takes the root of a Binary Tree
 // and returns an array containing the values from a post-order
 // traversal.
