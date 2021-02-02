02d528f74d278dc4bc8533b44e6bf90c1816e6f1
donovanwaddle <donovanwaddle@gmail.com>
2021-02-01 18:07:07 -0600

    Finished problem 4.

--- problems/04-bfs.js
+++ problems/04-bfs.js
@@ -2,8 +2,34 @@
 // binary tree, traverses it in depth-first-search
 // order, and returns an array containing the values
 // in the order you visited them.
-function bfs(root){
-    // Your code here
+function bfs(root, arr = []){
+    if (root === null) return [];
+
+    let queue = [root];
+
+    while(queue.length) {
+        let shifted = queue.shift();
+        arr.push(shifted.val);
+        if (shifted.left) {
+            queue.push(shifted.left);
+        }
+        if (shifted.right){
+            queue.push(shifted.right);
+        }
+    }
+
+    return arr;
+/*
+    push root
+    push left
+    push right
+
+    function(left)
+    return arr
+
+    [F, B, G, A, D]
+
+*/
 }
 
 module.exports = { bfs };
